07ef1567172c661e0467715f27b9ceb27a10ff7f
PES2UG23CS388 <nikitha8104@gmail.com>
2025-11-04 13:39:29 +0530

    Add files via upload

---
+++ Amusement_park/config/database.php
@@ -0,0 +1,20 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'amusement_park');
+
+function getDBConnection() {
+    $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    
+    if ($conn->connect_error) {
+        http_response_code(500);
+        die(json_encode([
+            'success' => false,
+            'message' => 'Database connection failed: ' . $conn->connect_error
+        ]));
+    }
+    
+    return $conn;
+}
+?>
